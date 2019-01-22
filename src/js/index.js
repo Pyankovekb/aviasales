10d91ac67aa1273a9fe70ec9e20f8dbd97ccbc42
Pyankovekb <pyankovekb@gmail.com>
2019-01-22 22:56:27 +0500

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -2,51 +2,89 @@ import $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 
+class Ticket {
+    constructor({price, stops, departure_time, departure_date,
+                 arrival_time, arrival_date, origin, origin_name,
+                    destination, destination_name}) {
+        this.price = price;
+        this.stops = stops;
+        this.departureTime = departure_time;
+        this.departureDate = departure_date;
+        this.arrivalTime = arrival_time;
+        this.arrivalDate = arrival_date;
+        this.origin = origin;
+        this.originName = origin_name;
+        this.destination = destination;
+        this.destinationName = destination_name;
+        this.oldPrice = price;
+    };
+
+    priceToggle(){
+        switch(getCurr()){
+            case 'USD':
+                this.price = Math.round(Number(this.oldPrice) * currObj.RUB_USD)+ ' USD';
+                break;
+            case 'EUR':
+                this.price = Math.round(Number(this.oldPrice) * currObj.RUB_EUR) + ' EUR';
+                break;
+            default: 
+                this.price = this.oldPrice + ' RUB';
+                break;
+        }
+    }
+}
+
+let ticketList = [];
+
+const updateLists = (list) => {
+    $('#ticket').empty();
+    list.filter(function(item){
+        let stops = item.stops;
+        return getStops().indexOf(String(stops)) >= 0;
+    }).sort(compare)
+    .forEach(function(item){
+        item.priceToggle();
+        render(item);
+    });
+};
+
+
 // окей, при изменении формы чекбоксов отправляем запрос
 // затем фильтруем и рендерим билеты 
 $('#stops').change(function(){
-    let ticketList;
     $.ajax({
         beforeSend: function() {
             $('#hiddenLoader').css('opacity', '1');
         },
         url: '../tickets.json',
         success: function(data) {
-            ticketList = [].concat(data.tickets);
+            ticketList = [];
+            data.tickets.forEach((item) => {
+                ticketList.push(new Ticket(item));
+            });
         },
-
         complete: function(){
             $('#ticket').empty();
             setTimeout(() => {                      // задержка для эмитации загрузки
-                ticketList.filter(function(item){
-                    let stops = item.stops;
-                    return getStops().indexOf(String(stops)) >= 0;
-                }).sort(compare)
-                .forEach(function(item){
-                    render(item);
-                });
+                updateLists(ticketList);
                 $('#hiddenLoader').css('opacity','0')
             }, 400);
-            
-            
         }
     });
-        //функция сортировки
-    function compare(a,b) {
-        if(a.price > b.price) return 1;
-        if(a.price < b.price) return -1;
-    };
 });
+//функция сортировки 
+function compare(a,b) {
+    if(a.price > b.price) return 1;
+    if(a.price < b.price) return -1;
+};
 // логика переключателей чекбоксов 
 $('#stops').click(function(element){
     let stopList = $(this).find('input'); 
-
     if(element.target.id == 'all' && $(element.target).prop('checked') == true) {
         for(let i = 0; i< stopList.length; i++) {
             $(stopList[i]).prop('checked', true);
         }
     } else if(element.target.id == 'all' && $(element.target).prop('checked', false)){
-
         for(let i = 0; i< stopList.length; i++) {
             $(stopList[i]).prop('checked', false);
         }
@@ -66,12 +104,12 @@ $('#stops').find('label').mouseover(function(event){
         }
     });
 });
-
 // при уходе с чекбокс спрятать SPAN 'только'(так лучше работает)
 $('#stops').find('label').mouseleave(function(event){
     let showOnly = $(event.target).find('span');
     showOnly.addClass('hidden');
 });
+
 // функция для получения активных чекбоксов для фильтрации
 function getStops() {
     let stopArr = [];
@@ -84,7 +122,6 @@ function getStops() {
 };
 
 //объект с котировками валют
-
 var currObj = {};
 $.ajax({
     url: 'https://free.currencyconverterapi.com/api/v6/convert?q=RUB_USD,RUB_EUR&compact=ultra',
@@ -108,8 +145,9 @@ $('#currency').click(function(event){
             $(cur[i]).attr('checked', false);
         }
     };
-    
     $(input).attr('checked', true);
+    ticketList.forEach((item) => item.priceToggle());
+    updateLists(ticketList);
 });
 // функция получения текущей валюты 
 function getCurr() {
@@ -125,85 +163,27 @@ function getCurr() {
     return currentCur;
 };
 
-// функция используется при рендере билетов
-// и выводит билеты сразу в выбраной валюте
-function curencyChecker(value) {
-    if(getCurr() === 'USD') {
-        return Math.round(Number(value) * Number(currObj.RUB_USD)) + ' USD';
-    } else if(getCurr() === 'EUR'){
-        return Math.round(Number(value) * Number(currObj.RUB_EUR)) + ' EUR';
-    } else {
-        return value + ' RUB'
-    }
-};
-// очень топорная функция переключения валют в билетах
-// как то лучше сделать я пока не додумал 
-$('#currency').change(function(){
-    let curr = getCurr();
-    if(curr === 'USD') {
-
-        if(($('#pricing').html()).indexOf('EUR') > 0){
-            console.log('FROM EUR TO USD');
-            $('* #pricing').html(function(){ 
-                return Math.round(parseInt($(this).html()) * 1.1382) + ' USD';
-            });
-        };
-        if(($('#pricing').html()).indexOf('RUB') > 0){
-            $('* #pricing').html(function(){ 
-                return Math.round(parseInt($(this).html()) * currObj.RUB_USD) + ' USD';
-            });
-        };
-    };
-    if(curr === 'EUR'){
-
-        if(($('#pricing').html()).indexOf('USD') > 0){
-            console.log('FROM USD TO EUR');
-            $('* #pricing').html(function(){ 
-                return Math.round(parseInt($(this).html()) / 1.1382) + ' EUR';
-            });
-        };
-        if(($('#pricing').html()).indexOf('RUB') > 0){
-            $('* #pricing').html(function(){ 
-                return Math.round(parseInt($(this).html()) * currObj.RUB_EUR) + ' EUR';
-            });
-        };
-    } 
-    if(curr ==='RUB') {
-
-        if(($('#pricing').html()).indexOf('USD') > 0){
-            $('* #pricing').html(function(){ 
-                return (Math.round(Math.round(parseInt($(this).html()) / currObj.RUB_USD)/100)) * 100 + ' RUB';
-            });
-        };
-        if(($('#pricing').html()).indexOf('EUR') > 0){
-            $('* #pricing').html(function(){ 
-                return (Math.round(Math.round(parseInt($(this).html()) / currObj.RUB_EUR)/100)) * 100 + ' RUB';;
-            });
-        };
-    };
-});
-
 // функция рендера билетов с шаблоном
 function render(item) {
     let myTicket = $(`<div class="ticket row">
-    <div class="col-lg-4 pricing">
-        <img src="img/turk.png">
-        <button id="buyTicket" class="btn btn-warning">Купить за <br><span id="pricing">${curencyChecker(item.price)}</span></button>
-    </div>
-    <div class="col-lg-8 flight row">
-        <div class="departure col">
-            <h2>${item.departure_time}</h2>
-            <p><b>${item.origin}, ${item.origin_name}</b></p>
-            <p>${dateFormat(item.departure_date)}</p>
-        </div>
-        <p class="transfer col">${stopsFormater(item.stops)}</p>
-        <div class="arrival col">
-            <h2>${item.arrival_time}</h2>
-            <p><b>${item.destination_name}, ${item.destination}</b></p>
-            <p>${dateFormat(item.arrival_date)}</p>
-        </div>
-    </div>
-</div>`);
+                        <div class="col-lg-4 pricing">
+                            <img src="../img/turk.png">
+                            <button id="buyTicket" class="btn btn-warning">Купить за <br><span id="pricing">${item.price}</span></button>
+                        </div>
+                        <div class="col-lg-8 flight row">
+                            <div class="departure col">
+                                <h2>${item.departureTime}</h2>
+                                <p><b>${item.origin}, ${item.originName}</b></p>
+                                <p>${dateFormat(item.departureDate)}</p>
+                            </div>
+                            <p class="transfer col">${stopsFormater(item.stops)}</p>
+                            <div class="arrival col">
+                                <h2>${item.arrivalTime}</h2>
+                                <p><b>${item.destinationName}, ${item.destination}</b></p>
+                                <p>${dateFormat(item.arrivalDate)}</p>
+                            </div>
+                        </div>
+                    </div>`);
     $('#ticket').append(myTicket);
 };
 
@@ -222,7 +202,6 @@ function dateFormat (date) {
 
 //функция форматирования строки с количеством пересадок
 function stopsFormater(stops) {
-    console.log(typeof stops);
     if(stops == 0 || stops > 4) {
         return stops + ' пересадок'
     } else if(stops == 1) {
